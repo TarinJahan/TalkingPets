0f339a47b62ba48c1fe388cbfc1c0f32f8721e68
Tarin <tarin@Zipcoders-MBP-5.hsd1.de.comcast.net>
2022-12-04 16:44:52 -0500

    created Pets class and subclasses and getters and setters

---
+++ src/main/java/io/zipcoder/polymorphism/Pets.java
@@ -0,0 +1,20 @@
+package io.zipcoder.polymorphism;
+
+public class Pets {
+
+    String name;
+    public Pets() {
+
+    }
+
+    public void setName(String name){
+    }
+
+    public String getName(String name){
+        return name;
+    }
+
+    public void speak(){
+    }
+
+}
